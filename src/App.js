8ab11ab9c9e2743a96a870cb4a68f02e44e7d3d8
Andrey Levchenko <andreiilevchenko@gmail.com>
2023-04-20 17:48:49 +0300

    add h-w-4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import {Link} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="App">APP
+      <ul>
+        <li>
+          <Link to={'/'}>Todos<Link/>
+        </li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   );
 }
